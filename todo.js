ec03b5f8c06a2b1dda74d00765236f4545ae96fe
mary <seraji.maryam1982@gmail.com>
2023-10-15 14:43:22 +0330

    some of changes done

--- todo.js
+++ todo.js
@@ -2,12 +2,12 @@ const save_button = document.querySelector("#save-btn");
 const title_input = document.querySelector("#title");
 const list = document.querySelector(".list");
 
-const todo_list = [];
+let todo_list = [];
 
-function makeItem(title){
+function renderItem(title){
     //create element div with class item
     const item = document.createElement("div");
-    item.classList.add("item");
+    item.classList.add("todo");
     
   
     //add checkbox to item class
@@ -20,33 +20,58 @@ function makeItem(title){
   
     item.appendChild(checkbox);
     item.appendChild(span);
+    
     list.appendChild(item);
 
 }
 function clearInput(){
     title_input.value = "";  
 }
+
+function renderList(){
+
+    for(let i=0; i < todo_list.length; i++){
+        const title = todo_list[i];
+        renderItem(title);
+       }
+}
 function syncStorage(title){
 
     todo_list.push(title);
     const next_list = JSON.stringify(todo_list);
+    console.log(todo_list, next_list)
     localStorage.setItem("my_list", next_list);
 }
+function loadFromStorage() {
+  const listFromStorage= JSON.parse(localStorage.getItem("my_list")) || [];
+  todo_list = listFromStorage;
+  } 
 
-save_button.addEventListener("click", () => {
- const value = title_input.value;
-//  console.log("clicked",value);
-if(value=== ""){
-    alert("You should write a text");
-}else {
-    
- 
-  syncStorage(value);
-  makeItem(value);
-  clearInput(value);
-//   console.log(todo_list) ;
-  
-//   console.log(my_collection);
+   
+
+function events(){
+
+    save_button.addEventListener("click", () => {
+        const value = title_input.value;
+       //  console.log("clicked",value);
+       if(value=== ""){
+           alert("You should write a text");
+       }else {
+                
+         syncStorage(value);
+         renderItem(value);
+         clearInput(value);
+       }
+       });
+       
 
 }
-})
+
+function init(){
+     loadFromStorage();
+     renderList();
+     events();    
+}
+
+init();
+
